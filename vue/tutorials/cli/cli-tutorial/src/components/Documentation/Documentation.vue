34faf33d3046afcd8b2e07173afcbc86d63d4ce9
PinkFlufflyLlama <Cocacola5010mc@gmail.com>
2020-02-27 22:30:31 +0000

    Used position fixed to overlay over over the header.

--- vue/tutorials/cli/cli-tutorial/src/components/Documentation/Documentation.vue
+++ vue/tutorials/cli/cli-tutorial/src/components/Documentation/Documentation.vue
@@ -26,7 +26,7 @@ export default {
 
 <style scoped>
    #container {
-      position: absolute;
+      position: fixed;
       width: 100%;
       height: 100%;
       background-color: rgba(0, 0, 0, 0.205);
